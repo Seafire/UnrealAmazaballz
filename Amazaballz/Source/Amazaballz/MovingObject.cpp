e4f75b73721e4470709e47e0757ecf933374e68d
Conor <ConorP95@hotmail.co.uk>
2016-11-11 23:44:56 +0000

    Update - It moves!

--- Amazaballz/Source/Amazaballz/MovingObject.cpp
+++ Amazaballz/Source/Amazaballz/MovingObject.cpp
@@ -10,7 +10,13 @@ AMovingObject::AMovingObject()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	root_ = CreateDefaultSubobject<UBoxComponent>(TEXT("Box"));
+	RootComponent = root_;
 
+	mesh_ = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	mesh_->AttachTo(RootComponent);
+
+	running_time_ = 0.0f;
 }
 
 // Called when the game starts or when spawned
@@ -18,10 +24,25 @@ void AMovingObject::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	//components_ = GetComponents().Array();
+
 	for (int i = 0; i < local_waypoints_.Num(); i++)
 	{
 		global_waypoints_.Emplace(local_waypoints_[i] + GetActorLocation());
 	}
+
+	/*if (components_.Num() != 0)
+	{
+		for (int i = 0; i < components_.Num(); i++)
+		{
+			UStaticMeshComponent* temp_static_mesh = Cast<UStaticMeshComponent>(components_[i]);
+
+			if (temp_static_mesh)
+			{
+				static_mesh_ = temp_static_mesh;
+			}
+		}
+	}*/
 }
 
 void AMovingObject::NextPoint()
@@ -68,7 +89,28 @@ void AMovingObject::Tick( float deltaTime )
 {
 	Super::Tick( deltaTime );
 
+	FVector new_location = GetActorLocation();
+	float delta_height = (FMath::Sin(running_time_ + deltaTime) - FMath::Sin(running_time_));
+	new_location.Y += (delta_height * speed_);
+	running_time_ += deltaTime;
+	SetActorLocation(new_location);
 
-	FVector velocity = CalculateMovement(deltaTime);
+	/*FVector velocity = CalculateMovement(deltaTime);
 	AddActorLocalOffset(velocity);
+
+	if (root_)
+	{
+		root_->SetWorldLocation(GetActorLocation());
+	}*/
+
+	//this->UpdateComponentTransforms();
+	
+	//if (static_mesh_)
+	//{
+	//	//static_mesh_->UpdateChildTransforms();
+	//	//if(static_mesh_->Transform)
+	//	static_mesh_->SetWorldTransform(GetTransform());
+	//	static_mesh_->AddLocalOffset(GetActorLocation() - static_mesh_->GetComponentLocation());
+	//}
+
 }
--- Amazaballz/Source/Amazaballz/MovingObject.h
+++ Amazaballz/Source/Amazaballz/MovingObject.h
@@ -14,35 +14,34 @@ class AMAZABALLZ_API AMovingObject : public AActor
 public:	
 	// Attributes.
 	UPROPERTY(EditAnywhere, Category = "Move Object")
-	float speed_;								// How fast will the object move between the points.
+	float speed_;										// How fast will the object move between the points.
 
 	UPROPERTY(EditAnywhere, Category = "Move Object")
-	TArray<FVector4> local_waypoints_;			// The points to move to.
+	TArray<FVector4> local_waypoints_;					// The points to move to.
+
+	UPROPERTY(EditAnywhere, Category = "Move Object")
+	UShapeComponent* root_;
+
+	UPROPERTY(EditAnywhere, Category = "Move Object")
+	UStaticMeshComponent* mesh_;
+
+	float running_time_;
 
 	// Sets default values for this actor's properties
 	AMovingObject();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Called every frame
+	virtual void Tick(float DeltaSeconds) override;
 
 	// This will setup the next point for the waypoints.
 	void NextPoint();
 
 	// This will calculate the next waypoint to move to.
 	FVector CalculateMovement(float tick);
-
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
 	
-	// Called every frame
-	virtual void Tick( float DeltaSeconds ) override;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Default")
-	class USceneComponent* actor_root_component_;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Default")
-	class UStaticMeshComponent* static_mesh_;
-
-	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Default")
-	//FBox box_collider_;
-
 	// Getters/Setters.
 	// This will allow us to get the current percentage to the next waypoint.
 	inline float get_percentage_to_waypoint() { return percentage_between_waypoints_; }
@@ -51,6 +50,7 @@ private:
 	// Attributes.
 	TArray<FVector4>  global_waypoints_;
 	//TArray<USceneComponent*> children_components_;
+	TArray<UActorComponent*> components_;
 	int from_waypoint_index_;
 	float percentage_between_waypoints_;
 
