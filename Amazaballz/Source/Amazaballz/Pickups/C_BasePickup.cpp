9676b46d695eda5c6145bef31a1f3e5c4db288ad
jmottershead94 <jasonmottershead@hotmail.co.uk>
2017-02-10 21:17:13 +0000

    Update - Implemented base pickup as an abstract class with pure virtual functions.

---
+++ Amazaballz/Source/Amazaballz/Pickups/C_BasePickup.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Amazaballz.h"
+#include "C_BasePickup.h"
+
+
+// Sets default values
+AC_BasePickup::AC_BasePickup()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AC_BasePickup::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AC_BasePickup::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
---
+++ Amazaballz/Source/Amazaballz/Pickups/C_BasePickup.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+/*
+
+	Base Pickup
+	===========
+
+	Created:	2017/02/12 20:58:10
+	Class Name: AC_BasePickup
+	Base Class:	AActor
+	Author(s):	Jason Mottershead
+
+	Purpose:	To provide a standard interface for all pickups in the game, all pickups
+				must implement logic based on what the base pickup defines.
+
+*/
+
+// Header guard.
+#pragma once
+
+// Includes here.
+#include "GameFramework/Actor.h"
+#include "C_BasePickup.generated.h"
+
+UCLASS(abstract)
+class AMAZABALLZ_API AC_BasePickup : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AC_BasePickup();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+protected:
+	UFUNCTION(BlueprintCallable, Category = "Pickups")
+		virtual void PickupResponse() PURE_VIRTUAL(AC_BasePickup::PickupResponse(), );
+
+	UFUNCTION(BlueprintCallable, Category = "Pickups")
+		virtual void PickupDestroy() PURE_VIRTUAL(AC_BasePickup::PickupDestroy(), );
+
+	
+};
