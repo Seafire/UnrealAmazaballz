39a53e8e7754e064ce9c1483779bea26ef1d6049
Conor <ConorP95@hotmail.co.uk>
2017-03-03 21:44:58 +0000

    Update - Set the camera for the basketball game mode.

---
+++ Amazaballz 4.15/Source/Amazaballz/C_Camera.h
@@ -0,0 +1,19 @@
+// Hilltown Games (C).
+
+#pragma once
+
+#include "Camera/CameraActor.h"
+#include "C_Camera.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AMAZABALLZ_API AC_Camera : public ACameraActor
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
